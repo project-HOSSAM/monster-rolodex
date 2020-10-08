8a4cbb12f4556901a2993f6b2b9cca5bfbb8f622
project HOSSAM <abouiba099@gmail.com>
2020-10-08 17:26:22 +0100

    converting from class based component to react hooks

--- src/App.js
+++ src/App.js
@@ -1,40 +1,38 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { CardList } from './components/card-list/card-list.component.jsx';
 
 import { SearchBox } from './components/search-box/search-box.component';
 
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      monsters: [],
-      searchField: ''
-    };
-  }
-  componentDidMount() {
+const App = () => {
+  const [monsters, setMonsters] = useState([]);
+  const [searchField, setSearchField] = useState('');
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ monsters: users }));
-  }
-  handleChange = e => this.setState({ searchField: e.target.value });
-  render() {
-    const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter(monster =>
-      monster.name.toLowerCase().includes(searchField.toLowerCase())
-    );
-    return (
-      <div className='App'>
-        <h1>MONSTERS ROLODEX</h1>
-        <SearchBox
-          placeholder='search monsters'
-          handleChange={this.handleChange}
-        />
-        <CardList monsters={filteredMonsters} />
-      </div>
-    );
-  }
-}
+      .then((response) => response.json())
+      .then((monsters) => setMonsters([...monsters]));
+  }, []);
+
+  const handleChange = (e) => {
+    setSearchField(e.target.value);
+  };
+
+  const filteredMonsters = monsters.filter((monster) =>
+    monster.name.toLowerCase().includes(searchField.toLowerCase())
+  );
+
+  return (
+    <div className='App'>
+      <h1>MONSTERS ROLODEX</h1>
+      <SearchBox
+        value={searchField}
+        placeholder='search monsters'
+        handleChange={handleChange}
+      />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
 
 export default App;
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 import './search-box.styles.css';
 
-export const SearchBox = ({ placeholder, handleChange }) => (
+export const SearchBox = ({ placeholder, handleChange, value }) => (
   <input
     className='search'
     type='search'
     placeholder={placeholder}
     onChange={handleChange}
+    value={value}
   />
 );
